43daba54b83018fa6da7a7c13b72558c49dfa053
Sagar Vadalia <sv344@cornell.edu>
2026-03-28 17:17:33 -0400

    photos

--- client/src/pages/PhotosPage.tsx
+++ client/src/pages/PhotosPage.tsx
@@ -71,6 +71,11 @@ const photos: Photo[] = [
   {id: '46', src: 'images/snorklin.avif', alt: 'snorkling in cancun', category: 'couple' },
   {id: '48', src: 'images/cosmohi.avif', alt: 'cosmo', category: 'lovedones' },
   {id: '49', src: 'images/michael.avif', alt: 'Michael', category: 'lovedones' },
+  {id: '50', src: 'images/nighttime-couple.avif', alt: 'nighttime biking', category: 'couple' },
+  {id: '51', src: 'images/old-photos.avif', alt: 'old photos', category: 'couple' },
+  {id: '52', src: 'images/couple-nyc-skyline.avif', alt: 'nyc skyline', category: 'couple' },
+  {id: '53', src: 'images/some-friends.avif', alt: 'friends at central park', category: 'lovedones' },
+
  ];
 
 export function PhotosPage() {
